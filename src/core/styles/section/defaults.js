7050c4f2db0c6a0dababf9e86cbef64a00c6a5d5
angelespejo <angelespejo13@gmail.com>
2023-03-15 23:15:08 +0100

    v1.0.0

--- src/core/styles/section/defaults.js
+++ src/core/styles/section/defaults.js
@@ -14,14 +14,9 @@ export class DefaultSection extends Section {
 
 		let styles = await this.getStorageStyles( )
 
-		console.log( 'Defaults',styles )
+		// console.log( 'Defaults',styles )
 
 		this.addDataObjToBody( styles )
-
-		// this.userText( styles['user']['text'] ? styles['user']['text'] : false )
-		// this.userColor( styles['user']['color'] ? styles['user']['color'] : false )
-		// this.chatText( styles['chat']['text'] ? styles['chat']['text'] : false )
-		// this.chatColor( styles['chat']['color'] ? styles['chat']['color'] : false )
 
 	}
 
